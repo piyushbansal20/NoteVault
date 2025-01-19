146422d71887ca6176a5117fbcf32cfe9a27a4a1
piyushbansal20 <115879708+piyushbansal20@users.noreply.github.com>
2025-01-19 15:52:52 +0530

    Update auth.js

--- backend/server/routes/auth.js
+++ backend/server/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('../models/User');
+const User = require('../models/user');
 
 
 passport.use(new GoogleStrategy({
